8a67919c4e1f40b746a7a5987a1f11f9b07cf0b8
Yui <95811710+SmolYui@users.noreply.github.com>
2022-03-24 15:29:56 +0000

    account object implemented

--- modules/economy.ts
+++ modules/economy.ts
@@ -1,8 +1,10 @@
 import balSchema from "../models/bal-schema";
 
+type account = {guildId:string,userId:string,bal:number}
+
 async function newAccount(key: string) {
     await balSchema.findOneAndUpdate({
-        id: key
+        _id: key
     }, {
         value: 0,
     }, {
@@ -11,37 +13,43 @@ async function newAccount(key: string) {
     console.log("created new balance account")
 }
 
-async function fetchAccount(key: string): Promise<number> {
+async function fetchAccount(key: string): Promise<account> {
     const results = await balSchema.findById(key)
     if (!results) {
         await newAccount(key)
         return fetchAccount(key)
     }
-    return results.value
+    const guildId = key.slice(0,18)
+    const userId = key.slice(18)
+    const bal = results.value
+    return {guildId,userId,bal}
 }
 
 export async function getBal(key: string): Promise<number> {
-    const bal = await fetchAccount(key)
+    const account = await fetchAccount(key)
+    const bal = account.bal
     return bal
 }
 
 export async function addBal(key: string, amount: number) {
-    const bal = await fetchAccount(key)
+    const account = await fetchAccount(key)
+    const bal = account.bal
     const add = amount > 0 ? amount : 0
     const newValue = bal + add
     await balSchema.findOneAndUpdate({
-        id: key
+        _id: key
     }, {
         value: newValue,
     })
 }
 
 export async function subBal(key: string, amount: number) {
-    const bal = await fetchAccount(key)
+    const account = await fetchAccount(key)
+    const bal = account.bal
     if (bal - amount < 0 ) return false
     const newValue = bal - amount
     await balSchema.findOneAndUpdate({
-        id: key
+        _id: key
     }, {
         value: newValue,
     })
